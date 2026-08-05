6ce6d5074bf08e2237e09596506868f057271ac1
sylas-rb <sylasrangel1999@gmail.com>
2026-08-04 21:43:03 -0300

    Department entity class

---
+++ demo-dao-jdbc/src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import Model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
+    }
+}
