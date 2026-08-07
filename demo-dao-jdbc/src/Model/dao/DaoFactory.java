4adadc440bfd1aca7ed1d977354b5eda1aa9d5f5
sylas-rb <sylasrangel1999@gmail.com>
2026-08-06 22:13:43 -0300

    findById implementation

--- demo-dao-jdbc/src/Model/dao/DaoFactory.java
+++ demo-dao-jdbc/src/Model/dao/DaoFactory.java
@@ -1,10 +1,10 @@
 package Model.dao;
 
 import Model.dao.impl.SellerDaoJDBC;
-import Model.entities.Seller;
+import db.DB;
 
 public class DaoFactory {
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
