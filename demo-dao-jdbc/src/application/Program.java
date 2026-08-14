a57db56aabd91cc4e808fb19c1083e999d258280
sylas-rb <sylasrangel1999@gmail.com>
2026-08-13 21:16:44 -0300

    Reusing instantiation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -2,18 +2,15 @@ package application;
 
 import Model.dao.DaoFactory;
 import Model.dao.SellerDao;
-import Model.entities.Department;
 import Model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println(" ==== TEST 1: seller findById =====");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
     }
 }
