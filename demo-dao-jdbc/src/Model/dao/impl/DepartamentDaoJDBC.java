dd41631e436c7c9a1b2c5359e067df81956dfde0
sylas-rb <sylasrangel1999@gmail.com>
2026-08-26 21:24:50 -0300

    department findById and department update

--- demo-dao-jdbc/src/Model/dao/impl/DepartamentDaoJDBC.java
+++ demo-dao-jdbc/src/Model/dao/impl/DepartamentDaoJDBC.java
@@ -44,7 +44,21 @@ public class DepartamentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("UPDATE department " +
+                    "SET Name = ? " +
+                    "WHERE id = ?");
 
+            st.setString(1, obj.getName());
+            st.setInt(2, obj.getId());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
@@ -54,7 +68,33 @@ public class DepartamentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT department.* " +
+                    "FROM department " +
+                    "WHERE department.id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Department obj = instantiateDepartment(rs);
+                return obj;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
     }
 
     @Override
--- demo-dao-jdbc/src/application/Program2.java
+++ demo-dao-jdbc/src/application/Program2.java
@@ -4,13 +4,26 @@ import Model.dao.DaoFactory;
 import Model.dao.DepartmentDao;
 import Model.entities.Department;
 
+import java.util.Scanner;
+
 public class Program2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println(" ==== TEST 1: department insert =====");
         Department department = new Department(null, "Story");
         departmentDao.insert(department);
         System.out.println("Inserted! new id = " + department.getId());
+
+        System.out.println(" ==== TEST 2: department findById ====");
+        department = departmentDao.findById(2);
+        System.out.println(department);
+
+        System.out.println(" ==== TEST 3: department update =====");
+        department = departmentDao.findById(7);
+        department.setName("BookStore");
+        departmentDao.update(department);
+        System.out.println("Updated! new name = " + department.getName());
     }
 }
